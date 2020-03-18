1021dd26edeffbc0b34ab1014f059ecb139e7c2f
Nikolay <samoil.nikolai@gmail.com>
2020-03-18 13:16:52 +0200

    addition 2

--- task2/les2.js
+++ task2/les2.js
@@ -94,23 +94,23 @@ calc(10, 8);
 function mathOperation(arg1, arg2, operation) {
     switch (operation) {
         case '+' :
-            console.log(arg1 + arg2);
-
+            // console.log(arg1 + arg2);
+            return arg1 + arg2;
             break;
         
         case '-' :
-            console.log(arg1 - arg2);
-    
+            // console.log(arg1 - arg2);
+            return arg1 - arg2;
             break;    
 
         case '/' :
-            console.log(arg1 / arg2);
-
+            // console.log(arg1 / arg2);
+            return arg1 / arg2;
             break;  
 
         case '*' :
-            console.log(arg1 * arg2);
-
+            // console.log(arg1 * arg2);
+            return arg1 * arg2;
             break;  
     }
 }
